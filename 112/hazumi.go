ec9b7aef42a57edfe8905b58c151111508d648a1
Noriaki Terakado <ntoreg@gmail.com>
2016-08-12 01:56:29 +0900

    [112] go fmt

--- 112/hazumi.go
+++ 112/hazumi.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
+	"fmt"
+	"strconv"
 )
 
 func isUp(n int) bool {
-    c := strconv.Itoa(n)
-    for i := 0; i < len(c); i++ {
-        if i + 1 >= len(c) {
-            continue
-        }
-        if c[i] > c[i+1] {
-            return false
-        }
-    }
-    return true
+	c := strconv.Itoa(n)
+	for i := 0; i < len(c); i++ {
+		if i+1 >= len(c) {
+			continue
+		}
+		if c[i] > c[i+1] {
+			return false
+		}
+	}
+	return true
 }
 
 func isDown(n int) bool {
-  c := strconv.Itoa(n)
-    for i := 0; i < len(c); i++ {
-        if i + 1 >= len(c) {
-            continue
-        }
-        if c[i] < c[i+1] {
-            return false
-        }
-    }
-    return true
+	c := strconv.Itoa(n)
+	for i := 0; i < len(c); i++ {
+		if i+1 >= len(c) {
+			continue
+		}
+		if c[i] < c[i+1] {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-    n := 0
-    x := 0
-    for {
-        n++
-        if !isUp(n) && !isDown(n) {
-            x++
-            per := float64(x) / float64(n)
-            if per == 0.99 {
-                fmt.Println(n)
-                break
-            }
-        }
-    }
+	n := 0
+	x := 0
+	for {
+		n++
+		if !isUp(n) && !isDown(n) {
+			x++
+			per := float64(x) / float64(n)
+			if per == 0.99 {
+				fmt.Println(n)
+				break
+			}
+		}
+	}
 }
 
 /*
